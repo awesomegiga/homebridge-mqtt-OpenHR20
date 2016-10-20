f17ce9f932babda04967b774a101fbe04f058f4d
awesomegiga <awesomegiga@outlook.com>
2016-10-20 22:43:53 +0200

    first version to be tested

---
+++ index.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var mqtt = require('mqtt');
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-mqtt-openhr20", "mqtt-openhr20", Thermostat_hr20);
+}
+
+function Thermostat_hr20(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.url = config['url'];
+  this.topic_CT = config['topic'] + '_CT';
+  this.topic_TT = config['topic'] + '_TT';
+  this.client_Id 		= 'mqttjs_' + Math.random().toString(16).substr(2, 8);7
+  this.options = {
+    keepalive: 10,
+    clientId: this.client_Id,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: 'WillMsg',
+      payload: 'Connection Closed abnormally..!',
+      qos: 0,
+      retain: false
+      },
+    username: config["username"],
+    password: config["password"],
+    rejectUnauthorized: false
+  };
+
+  this.temperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
+  var Current_temp;
+  var Target_temp;
+
+  this.client  = mqtt.connect(this.url, this.options);
+  var that = this;
+  this.client.subscribe(this.topic_TT);
+  this.client.subscribe(this.topic_CT);
+
+
+  this.client.on('message', function (topic_TT, message) {
+  data = JSON.parse(message);
+  if (data === null) {return null}
+  that.Target_temp = parseFloat(data);
+  });
+
+  this.client.on('message', function (topic_CT, message) {
+  data = JSON.parse(message);
+  if (data === null) {return null}
+  that.Current_temp = parseFloat(data);
+  });
+}
+
+Thermostat.prototype = {
+  getCurrentTemperature: function(callback) {
+    this.log(this.name, "- MQTT : Current Temprature = ", this.Current_temp)
+    callback(null, this.Current_temp);
+  },
+
+  setTargetTemperature: function(callback) {
+    this.log(this.name, "- MQTT : Current Temprature = ", this.Target_temp)
+    callback(null, this.Target_temp);
+  },
+
+  setTemperatureDisplayUnits: function(callback) {
+    this.log("setTemperatureDisplayUnits from %s to %s", this.temperatureDisplayUnits);
+    this.temperatureDisplayUnits = value;
+    callback(null, callback);
+}
+}
+
+AirQualityAccessory.prototype.getServices = function() {
+
+  var informationService = new Service.AccessoryInformation();
+
+informationService
+  .setCharacteristic(Characteristic.Manufacturer, "Honeywell Hacked")
+  .setCharacteristic(Characteristic.Model, "Open HR20")
+  .setCharacteristic(Characteristic.SerialNumber, "Open HR20 SN");
+
+var thermostatService = new Service.Thermostat(this.name);
+
+thermostatService
+  .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+  // .on('get', this.getCurrentHeatingCoolingState.bind(this));
+
+thermostatService
+  .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+  // .on('get', this.getTargetHeatingCoolingState.bind(this))
+  // .on('set', this.setTargetHeatingCoolingState.bind(this));
+
+thermostatService
+  .getCharacteristic(Characteristic.CurrentTemperature)
+  .on('get', this.getCurrentTemperature.bind(this));
+
+thermostatService
+  .getCharacteristic(Characteristic.TargetTemperature)
+ // .on('get', this.getTargetTemperature.bind(this))
+  .on('set', this.setTargetTemperature.bind(this));
+
+thermostatService
+  .getCharacteristic(Characteristic.TemperatureDisplayUnits)
+//  .on('get', this.getTemperatureDisplayUnits.bind(this))
+  .on('set', this.setTemperatureDisplayUnits.bind(this));
+
+  return [informationService, thermostatService];
+}
