2629c35b62a6190a788917bc127584774da2e929
awesomegiga <awesomegiga@outlook.com>
2016-11-20 17:44:40 +0100

    QoS 1

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function Thermostat_hr20(log, config) {
     will: {
       topic: 'WillMsg',
       payload: 'Connection Closed abnormally..!',
-      qos: 0,
+      qos: 1,
       retain: false
       },
     username: config["username"],
